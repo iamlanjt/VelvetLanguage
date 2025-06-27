9ede240cc0c345c5c6321e9e14e7f9b62b04b278
iamlanjt <new9gm@gmail.com>
2025-06-26 22:32:27 -0400

    Add more tokens, README, and multichar lexing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,92 @@
-use crate::token::VelvetToken;
+use crate::token::{VelvetToken, VelvetTokenType};
 
 mod token;
 
+// TODO: once done with all basic tokenizations, error on no end path reached & enable whitespace skipping
 fn tokenize(input: &str) -> Vec<VelvetToken> {
-    const 
+    let input_characters: Vec<char> = input.chars().clone().collect();
+    let mut tokenizer_index = 0;
+    let mut first = true;
+    let mut end_tokens: Vec<VelvetToken> = Vec::new();
+
+    while tokenizer_index < input_characters.len() - 1 {
+        if first == false { tokenizer_index = tokenizer_index + 1; }
+        first = false;
+        let mut current_char = input_characters[tokenizer_index];
+
+        if current_char.is_whitespace() {continue}
+
+        // Single char mapping
+        let token_result: Option<VelvetTokenType> = match current_char {
+            '+' => Some(VelvetTokenType::Plus),
+            '-' => Some(VelvetTokenType::Minus),
+            '*' => Some(VelvetTokenType::Asterisk),
+            '/' => Some(VelvetTokenType::Slash),
+            '=' => Some(VelvetTokenType::Eq),
+            _   => None
+        };
+        if token_result.is_some() {
+            end_tokens.push(VelvetToken {
+                kind: token_result.expect(""),
+                start_index: tokenizer_index,
+                end_index: tokenizer_index,
+                literal_value: current_char.to_string()
+            });
+            continue;
+        }
+
+        // Multi char processing
+        // Numbers
+        if current_char.is_numeric() && current_char != '0' {
+            let mut final_number = "".to_owned();
+            let start_index = tokenizer_index;
+            
+            while current_char.is_numeric() {
+                final_number = final_number + &current_char.to_string();
+                if tokenizer_index + 1 >= input_characters.len() {
+                    break
+                }
+                tokenizer_index += 1;
+                current_char = input_characters[tokenizer_index]
+            }
+
+            end_tokens.push(VelvetToken {
+                kind: VelvetTokenType::Number,
+                start_index,
+                end_index: tokenizer_index,
+                literal_value: final_number
+            })
+        }
+
+        // Identifiers
+        if current_char.is_alphabetic() {
+            let mut final_ident: String = "".to_owned();
+            let start_index: usize = tokenizer_index;
+
+            while current_char.is_alphanumeric() || current_char == '_' {
+                final_ident = final_ident + &current_char.to_string();
+                if tokenizer_index + 1 >= input_characters.len() {
+                    break
+                }
+                tokenizer_index += 1;
+                current_char = input_characters[tokenizer_index]
+            }
+
+            end_tokens.push(VelvetToken {
+                kind: VelvetTokenType::Identifier,
+                start_index,
+                end_index: tokenizer_index,
+                literal_value: final_ident
+            })
+        }
+    }
+
+    return end_tokens;
 }
 
 fn main() {
-    println!("Hello, world!");
+    let tokenizer_result = tokenize("const x = 1 + 2");
+    for this_token in tokenizer_result {
+        println!("Token {}  {}", this_token.kind, this_token.literal_value)
+    }
 }
--- src/token.rs
+++ src/token.rs
@@ -1,15 +1,37 @@
+use core::fmt;
+
+
+#[derive(Debug, Copy, Clone)]
 pub enum VelvetTokenType {
     Plus,
     Minus,
     Asterisk,
     Slash,
     Identifier,
-    Number
+    Number,
+    Eq
 }
 
+const VTOK_EQUIV: [&str; 7] = [
+    "Plus",
+    "Minus",
+    "Asterisk",
+    "Slash",
+    "Identifier",
+    "Number",
+    "Equals"
+];
+
+impl fmt::Display for VelvetTokenType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", VTOK_EQUIV[*self as usize])
+    }
+}
+
+#[derive(Debug)]
 pub struct VelvetToken {
-    kind: VelvetTokenType,
-    startIndex: i64,
-    endIndex: i64,
-    literalValue: String
+    pub kind: VelvetTokenType,
+    pub start_index: usize,
+    pub end_index: usize,
+    pub literal_value: String
 }
